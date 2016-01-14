8ffbb8412fd5faf99878f29901136892d3fbe49e
ctevnan <carolyntevnan@yahoo.com>
2016-01-13 20:34:55 -0500

    added credentials to keys.js

--- keys.js
+++ keys.js
@@ -1,7 +1,7 @@
 console.log('this is loaded');
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'CwNiDnzHIEhRFKy64Ct8ISFaN',
+  consumer_secret: 'SCtAaQidTB9wcI4qMP5Ak7QHfT9PVrxO36Q2JwXT2llvmRQZrn',
+  access_token_key: '4062146535-kpqELDbnVTlzc10ra7uuC9R6gSlFM5leXErRuVP',
+  access_token_secret: '5EMBFtYs16l9TaeVGvts0B7EOJ4FNnSKTfahuwMvxyVSh',
 }
